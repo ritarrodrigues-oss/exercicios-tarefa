793c0e31d474e509b8cf9c10f242270566764be2
ritarodrigues-oss <rita_r_rodrigues@estudante.sesisenai.org.br>
2026-09-22 19:16:49 -0300

    atualizações

---
+++ variaveis-parte1/exercio-2.py
@@ -0,0 +1,7 @@
+# Pedir 2 numeros e exibir a soma dos dois
+
+num1 = int(input("Digite o primeiro numero: "))
+num2 = int(input("Digite o segundo numero: "))
+soma = num1 + num2
+
+print("A soma dos dois numeros é: ", soma)
